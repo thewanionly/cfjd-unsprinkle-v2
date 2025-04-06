c1788fe5a1b34cfb70331c216055fca142cfc13d
thewanionly <pelloani@gmail.com>
2025-04-07 05:52:02 +0800

    exercise-4:  fix tag overflow

--- src/components/PhotoGridItem/PhotoGridItem.jsx
+++ src/components/PhotoGridItem/PhotoGridItem.jsx
@@ -50,9 +50,10 @@ const Image = styled.img`
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  padding: 4px 0;
 `;
 
 const Tag = styled.li`
@@ -61,6 +62,12 @@ const Tag = styled.li`
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+
+  &:not(:last-of-type) {
+    margin-right: 8px;
+  }
+
+  display: inline;
 `;
 
 export default PhotoGridItem;
